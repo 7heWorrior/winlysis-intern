260deffe06b341134320a6e4d3a21e5e4ff807ed
7heWorrior <manas.mahajan17@vit.edu>
2020-07-05 17:56:49 +0530

    deployment

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const app = express()
 const shortid= require('shortid')
 const port = process.env.PORT || 3000
 const {Storage} = require('@google-cloud/storage');
+const path = require('path')
 
+const bucketName = "manas-mahajan-123"
 
 app.use(express.json())
 
 
 app.post('/users',async (req,res)=>{
-      const filename = "./users/" + shortid.generate() + ".json"
+      const filename = path.join( __dirname,"./users/" + shortid.generate() + ".json")
       const content = JSON.stringify(req.body)
       fs.writeFileSync(filename,content)
-      await saveToCloud('manas-mahajan-123',filename)
+      await saveToCloud(bucketName,filename)
       res.send(filename)
 })
 
